4d529b7f81fd105b7ec6d51d870b75b157cd29a7
gougoulias <j_gougoulias@hotmail.com>
2021-05-07 20:01:19 +0300

    separate multiple values eg checkbox
    separate multiple values eg checkbox

--- cf-stats.php
+++ cf-stats.php
@@ -37,30 +37,18 @@ function cf_stats_plugin($atts){
 		);
 
 		$flamingo_posts=get_posts($args);
-		// echo "<br>flamingo posts :<br>";
-		// print_r($flamingo_posts);
-		// echo "<br>flamingo posts ends :<br>";
-
-		//prepare regular expression
-		$pattern = "/_field_/i";
-
-		//$temp_group= array();
+		
 		//get the fields values
 		foreach ($flamingo_posts as $flp) {
-			//echo "<br>flamingo post start<br>";
-			// echo "<br>flamingo posts meta :<br>";
-			// print_r(get_post_meta($flp));
-			// echo "<br>flamingo posts meta ends :<br>";
-			// echo '</br>-----<br>';
 
-			//echo "<br>get taxonomies: <br> ";
+			//get taxonomies 
 			$flamingo_taxonomy=wp_get_object_terms($flp,'flamingo_inbound_channel');
 			//print_r($flamingo_taxonomy);
 			foreach ($flamingo_taxonomy as $fltax) {
 			 	$flamingo_taxonomy_name=$fltax->name;
 			 } 
 			//echo "this post belongs to form with name : " . $flamingo_taxonomy_name ;
-			//echo "<br>get taxonomies ends<br>";
+			//get taxonomies ends
 			
 			if ($flamingo_taxonomy_name==$name){
 				echo "<br>Flamingo post start<br>";
@@ -76,54 +64,42 @@ function cf_stats_plugin($atts){
 							$group_field.=(string)$sgroup;
 							//echo $group_field;
 							if($fkey==$group_field){
-								echo " einai group <br>";
-								echo "<br>--------group starts HERE--------------<br>";
+								//echo " einai group <br>";
+								echo "<br>--------group  ". $group_field ." starts HERE--------------<br>";
 								$group_name=substr($fkey,7);
-								echo "GROUP NAME = ". $group_name . '<br>';
+								//echo "GROUP NAME = ". $group_name . '<br>';
 								$value=get_post_meta($flp)[$fkey][0];
 								$regex_value='(.*:")';
 								$replacement = '$1';
 								$newvalue= preg_replace($regex_value, $replacement, $value);
 								$regex_value='(".*)';
 								$newvalue= preg_replace($regex_value, $replacement, $newvalue);
-								echo 'kathari timi = ' . $newvalue .'<br>';
+								//echo 'kathari timi = ' . $newvalue .'<br>';
 								//second loop starts here
 								foreach ($get_the_keys as $fkeygrouped) {
 									if (preg_match('/_field_/i', $fkeygrouped) ){
-										echo 'to kleidi einai : ' .$fkeygrouped . '<br>';
+										//echo 'to kleidi einai : ' .$fkeygrouped . '<br>';
 										print_r(get_post_meta($flp)[$fkeygrouped]);
 										echo '</br>';
 										$valuegrouped=get_post_meta($flp)[$fkeygrouped][0];
-											// $regex_value='(.*:")';
-											// $replacement = '$1';
-											// $newvaluegrouped= preg_replace($regex_value, $replacement, $valuegrouped);
-											// $regex_value='(".*)';
-											// $newvaluegrouped= preg_replace($regex_value, $replacement, $newvaluegrouped);
-											// echo 'kathari timi new value= ' . $newvaluegrouped .'<br>';
-										//keep the values to the array with the newvalue
-										//$groups_array[$group_name][$newvalue][$fkeygrouped][]=get_post_meta($flp)[$fkeygrouped];
-										//$groups_array[$group_name][$newvalue][$fkeygrouped][]=$newvaluegrouped;
-										$groups_array[$group_name][$newvalue][$fkeygrouped][]=$valuegrouped;
+										//yparxei problima an i timi den dinetai me ayti tin morfi opws px to subject i to onoma an ypaxei genika ta text fields
+										$regex_value='/("[a-zA-Z0-9?-?+]+")/i';
+										preg_match_all($regex_value, $valuegrouped, $matches_grouped);
+										$groups_array[$group_name][$newvalue][$fkeygrouped][]=$matches_grouped[1];
 									}
 								}
-								echo "<br>--------group ENDS HERE--------------<br>";
+								echo "<br>--------group ". $group_field ." ENDS HERE--------------<br>";
 							}
 						}//end grouping
 
-						echo 'to kleidi einai : ' .$fkey . '<br>';
+						//echo 'to kleidi einai : ' .$fkey . '<br>';
 						print_r(get_post_meta($flp)[$fkey]);
 						echo '</br>';
 						$value_ungrouped=get_post_meta($flp)[$fkey][0];
-							// $regex_value='(.*:")';
-							// $replacement = '$1';
-							// $newvalue_ungrouped= preg_replace($regex_value, $replacement, $value_ungrouped);
-							// $regex_value='(".*)';
-							// $newvalue_ungrouped= preg_replace($regex_value, $replacement, $newvalue_ungrouped);
-							// echo 'kathari timi ungrouped = ' . $newvalue_ungrouped .'<br>';
-						// keeps the values to the 'ungrouped' array
-						//$groups_array['ungrouped']['ungrouped'][$fkey][]=get_post_meta($flp)[$fkey];
-						// $groups_array['ungrouped'][$fkey][]=$newvalue_ungrouped;
-						$groups_array['ungrouped']['ungrouped'][$fkey][]=$value_ungrouped;
+						//yparxei problima an i timi den dinetai me ayti tin morfi opws px to subject i to onoma an ypaxei genika ta text fields
+						$regex_value='/("[a-zA-Z0-9?-?+]+")/i';
+						preg_match_all($regex_value, $value_ungrouped, $matches_ungrouped);
+						$groups_array['ungrouped']['ungrouped'][$fkey][]=$matches_ungrouped[1];
 						echo "<br>";
 						
 					}
@@ -135,9 +111,9 @@ function cf_stats_plugin($atts){
 		echo "<br>The shortcode parameter 'name=' is required <br>";
 	}
 	echo 'groups arrays is <br>';
-	echo'<pre>';
+	//echo'<pre>';
 	print_r($groups_array);
-	echo'</pre>';
+	//echo'</pre>';
 	// how to count 1st try starts
 	echo '<br>counting starts<br><pre>';
 	foreach ($groups_array as $ga) {
@@ -146,11 +122,11 @@ function cf_stats_plugin($atts){
 			//print_r($cgroups);
 			foreach ($cgroups as $cvalues) {
 				//print_r($cvalues);
-				print_r(array_count_values($cvalues));
-				// foreach ($cvalues as $cv) {
+				//print_r(array_count_values($cvalues));
+				 foreach ($cvalues as $cv) {
 				// 	//print_r($cv);
-				// 	print_r(array_count_values($cv));
-				// }
+					print_r(array_count_values($cv));
+				 }
 			}	
 		}	
 	}
