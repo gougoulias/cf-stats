c26cc77420b2c3c90d49a452238321a95f5a2b25
Giannis Gougoulias <j_gougoulias@hotmail.com>
2021-05-08 20:22:22 +0300

    shortcode parameter for counting elements
    shortcode parameter for counting elements and check if a there is a counting element given in the form[also for grouping]

--- cf-stats.php
+++ cf-stats.php
@@ -14,9 +14,14 @@ function cf_stats_plugin($atts){
 		// group = the fileds that need to be grouped
 	extract(shortcode_atts(array(
 		'name'=> '',
+		'stats'=>'',
 		'group'=>'',	
 	),$atts));
 
+	if ($stats!=null){
+	$allstats=explode(',',$stats);
+	}
+	
 	$allgroups=explode(',', $group);
 	//echo "all groups: ";
 	//create empty array for each group that is reported in the shortcode parameter
@@ -28,7 +33,7 @@ function cf_stats_plugin($atts){
 
 
 	// check if the name parameter is given in the shortcode
-	if ($name!=null){
+	if ($name!=null && $stats!=null){
 
 		// set up the post arguments to get all the flamingo inbounds port types IDs and store them to flamingo_post variable
 		$args =array(
@@ -57,58 +62,71 @@ function cf_stats_plugin($atts){
 				//$isgroup=false;
 				foreach ($get_the_keys as $fkey) {
 					if (preg_match('/_field_/i', $fkey) ){
-						
-						//start grouping
-						foreach ($allgroups as $sgroup) {
-							$group_field='_field_';
-							$group_field.=(string)$sgroup;
-							//echo $group_field;
-							if($fkey==$group_field){
-								//echo " einai group <br>";
-								echo "<br>--------group  ". $group_field ." starts HERE--------------<br>";
-								$group_name=substr($fkey,7);
-								//echo "GROUP NAME = ". $group_name . '<br>';
-								$value=get_post_meta($flp)[$fkey][0];
-								$regex_value='(.*:")';
-								$replacement = '$1';
-								$newvalue= preg_replace($regex_value, $replacement, $value);
-								$regex_value='(".*)';
-								$newvalue= preg_replace($regex_value, $replacement, $newvalue);
-								//echo 'kathari timi = ' . $newvalue .'<br>';
-								//second loop starts here
-								foreach ($get_the_keys as $fkeygrouped) {
-									if (preg_match('/_field_/i', $fkeygrouped) ){
-										//echo 'to kleidi einai : ' .$fkeygrouped . '<br>';
-										print_r(get_post_meta($flp)[$fkeygrouped]);
-										echo '</br>';
-										$valuegrouped=get_post_meta($flp)[$fkeygrouped][0];
-										//yparxei problima an i timi den dinetai me ayti tin morfi opws px to subject i to onoma an ypaxei genika ta text fields
-										$regex_value='/("[\w\d\sαβγδεζηθικλμνξοπρστυφχψωςΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩάέήίόύώΆΈΉΊΌΎΏϊϋΪΫ&?-?-]+")/i';
-										preg_match_all($regex_value, $valuegrouped, $matches_grouped);
-										$groups_array[$group_name][$newvalue][$fkeygrouped][]=$matches_grouped[1];
-									}
-								}
-								echo "<br>--------group ". $group_field ." ENDS HERE--------------<br>";
-							}
-						}//end grouping
 
-						//echo 'to kleidi einai : ' .$fkey . '<br>';
-						print_r(get_post_meta($flp)[$fkey]);
-						echo '</br>';
-						$value_ungrouped=get_post_meta($flp)[$fkey][0];
-						//yparxei problima an i timi den dinetai me ayti tin morfi opws px to subject i to onoma an ypaxei genika ta text fields
-						$regex_value='/("[\w\d\sαβγδεζηθικλμνξοπρστυφχψωςΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩάέήίόύώΆΈΉΊΌΎΏϊϋΪΫ&?-?-]+")/i';
-						preg_match_all($regex_value, $value_ungrouped, $matches_ungrouped);
-						$groups_array['ungrouped']['ungrouped'][$fkey][]=$matches_ungrouped[1];
-						echo "<br>";
-						
+						//print_r($allstats);
+						foreach ($allstats as $statvalue) {
+							$stat_field='_field_';
+							$stat_field.=(string)$statvalue;
+							if($fkey==$stat_field){ //elegxos an to field prepei na metrithei
+								
+								//start grouping
+								foreach ($allgroups as $sgroup) {
+									$group_field='_field_';
+									$group_field.=(string)$sgroup;
+									//echo $group_field;
+									if($fkey==$group_field){//elegxos an anikei se group
+										//echo " einai group <br>";
+										echo "<br>--------group  ". $group_field ." starts HERE--------------<br>";
+										$group_name=substr($fkey,7);
+										//echo "GROUP NAME = ". $group_name . '<br>';
+										$value=get_post_meta($flp)[$fkey][0];
+										$regex_value='(.*:")';
+										$replacement = '$1';
+										$newvalue= preg_replace($regex_value, $replacement, $value);
+										$regex_value='(".*)';
+										$newvalue= preg_replace($regex_value, $replacement, $newvalue);
+										//echo 'kathari timi = ' . $newvalue .'<br>';
+										//second loop starts here
+										foreach ($get_the_keys as $fkeygrouped) {
+											if (preg_match('/_field_/i', $fkeygrouped) ){
+												foreach ($allstats as $statvalue) {
+													$stat_field_grouped='_field_';
+													$stat_field_grouped.=(string)$statvalue;
+													if($fkeygrouped==$stat_field_grouped){ //elegxos an to field prepei na metrithei gia ta grouped
+														//echo 'to kleidi einai : ' .$fkeygrouped . '<br>';
+														//print_r(get_post_meta($flp)[$fkeygrouped]);
+														//echo '</br>';
+														$valuegrouped=get_post_meta($flp)[$fkeygrouped][0];
+														$regex_value='/("[\w\d\sαβγδεζηθικλμνξοπρστυφχψωςΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩάέήίόύώΆΈΉΊΌΎΏϊϋΪΫ&?-?-]+")/i';
+														preg_match_all($regex_value, $valuegrouped, $matches_grouped);
+														$all_matches_grouped= implode(", ",$matches_grouped[1]);
+														$groups_array[$group_name][$newvalue][$fkeygrouped][]=$all_matches_grouped;
+													}
+												}	
+											}
+										}
+										echo "<br>--------group ". $group_field ." ENDS HERE--------------<br>";
+									}
+								}//end grouping
+
+								//echo 'to kleidi einai : ' .$fkey . '<br>';
+								//print_r(get_post_meta($flp)[$fkey]);
+								//echo '</br>';
+								$value_ungrouped=get_post_meta($flp)[$fkey][0];
+								$regex_value='/("[\w\d\sαβγδεζηθικλμνξοπρστυφχψωςΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩάέήίόύώΆΈΉΊΌΎΏϊϋΪΫ&?-?-]+")/i';
+								preg_match_all($regex_value, $value_ungrouped, $matches_ungrouped);
+								$all_matches_ungrouped=implode(", ", $matches_ungrouped[1]);
+								$groups_array['ungrouped']['ungrouped'][$fkey][]=$all_matches_ungrouped;
+								echo "<br>";
+							}
+						}	
 					}
 				}
 				echo "<br>Flamingo post Ends<br>";
 			}
 		}
 	}else{
-		echo "<br>The shortcode parameter 'name=' is required <br>";
+		echo "<br>The shortcode parameter 'name=' and 'stats=' is required <br>";
 	}
 	echo 'groups arrays is <br>';
 	//echo'<pre>';
@@ -122,11 +140,11 @@ function cf_stats_plugin($atts){
 			//print_r($cgroups);
 			foreach ($cgroups as $cvalues) {
 				//print_r($cvalues);
-				//print_r(array_count_values($cvalues));
-				 foreach ($cvalues as $cv) {
-				// 	//print_r($cv);
-					print_r(array_count_values($cv));
-				 }
+				print_r(array_count_values($cvalues));
+				//  foreach ($cvalues as $cv) {
+				// // 	//print_r($cv);
+				// 	print_r(array_count_values($cv));
+				//  }
 			}	
 		}	
 	}
