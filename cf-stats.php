fa9938c024426a88e45a0490053284222aaad7fd
Giannis Gougoulias <j_gougoulias@hotmail.com>
2021-05-15 18:47:08 +0300

    Fix limit in args for get_posts
    Fix limit in args for get_posts

--- cf-stats.php
+++ cf-stats.php
@@ -24,15 +24,10 @@ function cf_stats_plugin($atts){
 		$allstats=explode(',',$stats);
 	}
 	
-	$allgroups=explode(',', $group);
-	//echo "all groups: ";
-	//create empty array for each group that is reported in the shortcode parameter
-	foreach ($allgroups as $sgroup) {
-		//echo $sgroup . ', ';
-		$groups_array=array();
-		$groups_array[$sgroup]=array();
+	if ($group!=null){
+		//make an array with all the groups reported in the shortcode parameters
+		$allgroups=explode(',', $group);
 	}
-
 
 	// check if the name parameter and the stats parametere is given in the shortcode
 	if ($name!=null && $stats!=null){
@@ -41,13 +36,13 @@ function cf_stats_plugin($atts){
 		$args =array(
 			'post_type'=> 'flamingo_inbound',
 			'fields'=> 'ids',
+			'nopaging'=> true,
 		);
 
 		$flamingo_posts=get_posts($args);
 		
 		//get the fields values
 		foreach ($flamingo_posts as $flp) {
-
 			//get taxonomies 
 			$flamingo_taxonomy=wp_get_object_terms($flp,'flamingo_inbound_channel');
 			//print_r($flamingo_taxonomy);
@@ -72,58 +67,59 @@ function cf_stats_plugin($atts){
 							$stat_field='_field_';
 							$stat_field.=(string)$statvalue;
 							if($fkey==$stat_field){ // check if the field is countable or not
-								
-								//start grouping
-								foreach ($allgroups as $sgroup) {
-									$group_field='_field_';
-									$group_field.=(string)$sgroup;
-									//echo $group_field;
-									if($fkey==$group_field){// check if the field is grouped or not
-										//echo "<br>--------group  ". $group_field ." starts HERE--------------<br>";
-										$group_name=substr($fkey,7);
-										// get the value of the form field
-										$value=get_post_meta($flp)[$fkey][0];
-										//clean the value of not essential elements for the grouping face start
-										$regex_value='(.*:")';
-										$replacement = '$1';
-										$newvalue= preg_replace($regex_value, $replacement, $value);
-										$regex_value='(".*)';
-										$newvalue= preg_replace($regex_value, $replacement, $newvalue);
-										//clean the value of not essential elements for the grouping face ends
-										//echo 'kathari timi = ' . $newvalue .'<br>';
-										//second loop of the form fields starts here
-										foreach ($get_the_keys as $fkeygrouped) {
-											//check if the key is form input field or other type of form information
-											if (preg_match('/_field_/i', $fkeygrouped) ){
-												foreach ($allstats as $statvalue_grouped) {
-													$stat_field_grouped='_field_';
-													$stat_field_grouped.=(string)$statvalue_grouped;
-													if($fkeygrouped==$stat_field_grouped){ // check if the field is countable or not for the grouped value
-														//echo 'to kleidi einai : ' .$fkeygrouped . '<br>';
-														//print_r(get_post_meta($flp)[$fkeygrouped]);
-														//echo '</br>';
-														// get the value of the form field for the grouuped value
-														$valuegrouped=get_post_meta($flp)[$fkeygrouped][0];
-														$regex_value='/("[\w\d\sαβγδεζηθικλμνξοπρστυφχψωςΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩάέήίόύώΆΈΉΊΌΎΏϊϋΪΫ&+?-?-]+")/i';
-														//clean the value of not essential elements 
-														preg_match_all($regex_value, $valuegrouped, $matches_grouped);
-														//separate multiple values e.g. multiselect or checkbox
-														$separate_matches_grouped= implode(", ",$matches_grouped[1]);
-														//create array for all grouped values
-														$all_matches_grouped= explode(", ",$separate_matches_grouped);
-														foreach ($all_matches_grouped as $fin_value_grouped) {
-															// store the grouped values to the array
-															$groups_array[$group_name][$newvalue][$statvalue_grouped][]=$fin_value_grouped;
-															//create array and store the count of each values per group
-															$counted[$group_name][$newvalue][$statvalue_grouped]=array_count_values($groups_array[$group_name][$newvalue][$statvalue_grouped]);
+								if ($allgroups){
+									//start grouping
+									foreach ($allgroups as $sgroup) {
+										$group_field='_field_';
+										$group_field.=(string)$sgroup;
+										//echo $group_field;
+										if($fkey==$group_field){// check if the field is grouped or not
+											//echo "<br>--------group  ". $group_field ." starts HERE--------------<br>";
+											$group_name=substr($fkey,7);
+											// get the value of the form field
+											$value=get_post_meta($flp)[$fkey][0];
+											//clean the value of not essential elements for the grouping face start
+											$regex_value='(.*:")';
+											$replacement = '$1';
+											$newvalue= preg_replace($regex_value, $replacement, $value);
+											$regex_value='(".*)';
+											$newvalue= preg_replace($regex_value, $replacement, $newvalue);
+											//clean the value of not essential elements for the grouping face ends
+											//echo 'kathari timi = ' . $newvalue .'<br>';
+											//second loop of the form fields starts here
+											foreach ($get_the_keys as $fkeygrouped) {
+												//check if the key is form input field or other type of form information
+												if (preg_match('/_field_/i', $fkeygrouped) ){
+													foreach ($allstats as $statvalue_grouped) {
+														$stat_field_grouped='_field_';
+														$stat_field_grouped.=(string)$statvalue_grouped;
+														if($fkeygrouped==$stat_field_grouped){ // check if the field is countable or not for the grouped value
+															//echo 'to kleidi einai : ' .$fkeygrouped . '<br>';
+															//print_r(get_post_meta($flp)[$fkeygrouped]);
+															//echo '</br>';
+															// get the value of the form field for the grouuped value
+															$valuegrouped=get_post_meta($flp)[$fkeygrouped][0];
+															$regex_value='/("[\w\d\sαβγδεζηθικλμνξοπρστυφχψωςΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩάέήίόύώΆΈΉΊΌΎΏϊϋΪΫ&+?-?-]+")/i';
+															//clean the value of not essential elements 
+															preg_match_all($regex_value, $valuegrouped, $matches_grouped);
+															//separate multiple values e.g. multiselect or checkbox
+															$separate_matches_grouped= implode(", ",$matches_grouped[1]);
+															//create array for all grouped values
+															$all_matches_grouped= explode(", ",$separate_matches_grouped);
+															foreach ($all_matches_grouped as $fin_value_grouped) {
+																// store the grouped values to the array
+																$groups_array[$group_name][$newvalue][$statvalue_grouped][]=$fin_value_grouped;
+																//create array and store the count of each values per group
+																$counted[$group_name][$newvalue][$statvalue_grouped]=array_count_values($groups_array[$group_name][$newvalue][$statvalue_grouped]);
+															}
 														}
-													}
-												}	
-											}
-										}//second loop of the form fiedls ends here
-										//echo "<br>--------group ". $group_field ." ENDS HERE--------------<br>";
-									}
-								}//end grouping
+													}	
+												}
+											}//second loop of the form fiedls ends here
+											//echo "<br>--------group ". $group_field ." ENDS HERE--------------<br>";
+										}
+									}//end grouping
+								}//end ckeck for groups
 								//print_r(get_post_meta($flp)[$fkey]);
 								// get the value of the form field for the ungrouuped value
 								$value_ungrouped=get_post_meta($flp)[$fkey][0];
@@ -152,29 +148,19 @@ function cf_stats_plugin($atts){
 		//print message to user to use the shortcode parameters name and stats as they are required
 		echo "<br>The shortcode parameter 'name=' and 'stats=' is required <br>";
 	}
-	echo 'groups arrays is <br>';
-	echo'<pre>';
-	print_r($groups_array);
-	echo'</pre>';
+	// echo 'groups arrays is <br>';
+	// echo'<pre>';
+	// print_r($groups_array);
+	// echo'</pre>';
 
-	//couning starts
-	//echo '<br>counting  VIEW Starts<br>';
-	//echo'<pre> count ungrouped <br>';
-	//print_r($count_ungrouped);
-	//echo'</pre>';
-	//echo'<pre> count grouped <br>';
-	//print_r($count_grouped);
-	//echo'</pre>';
-	//echo '<br>counting VIEW ends<br>';
-	// how to count 1st try ends
-	
+	// //make the array json format
+	// $json_records=json_encode($groups_array);
+	// echo 'this is the json :<br>' . $json_records .'<br>';
+
 	//include('visualization.php');
 	//include('temp_visualization.php');
 	include('visual.php');
 	
-	//make the array json format
-	// $json_records=json_encode($groups_array);
-	// echo 'this is the json :<br>' . $json_records .'<br>';
 }
 
 add_shortcode('cf-stats','cf_stats_plugin');
