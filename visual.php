55664bed5ec4b9f4f55b609afddd9ad359958fbd
Giannis Gougoulias <j_gougoulias@hotmail.com>
2021-05-15 19:50:12 +0300

    add comments on visual.php
    add comments on visual.php

--- visual.php
+++ visual.php
@@ -1,63 +1,41 @@
 <?php
-
-$group_count=0;
-$max_final_questions=0;
+// call the array counted to assign the values so it can be viewable in the script adding label and y keys START
 foreach ($counted as $parrent_group => $grouped1) {
-	//echo '<h3>'.$parrent_group.'</h3>';
+	//echo '<p>'.$parrent_group.'</p>';
 	foreach ($grouped1 as $groupped_value => $grouped2 ){
 		//echo '<p>' . $groupped_value. '</p>';
 		$group_count=$group_count+1;
-		$final_questions=0;
 		foreach ($grouped2 as $onegroup => $onegroupvalue) {
-			//echo '<small>'.$onegroup.'</small> | ';
-			$final_questions=$final_questions+1;
 			$i=0;//counter for the answers
-			(string)$group_name='groupname';
-			(string)$group_name=(string)$group_name.(string)$group_count;
-			// echo 'group name is : ' .$group_name;
-			// echo ' | onegroup is : ' . $onegroup;
 			foreach ($onegroupvalue as $label => $y) {
-					$dataPoints[$group_name][$parrent_group][$groupped_value][$onegroup][$i]["label"]=$label;
-					$dataPoints[$group_name][$parrent_group][$groupped_value][$onegroup][$i]["y"]=$y;
-					$dataPointsdemo[$groupped_value][$onegroup][$i]["label"]=$label;
-					$dataPointsdemo[$groupped_value][$onegroup][$i]["y"]=$y;
+					$dataPoints[$groupped_value][$onegroup][$i]["label"]=$label;
+					$dataPoints[$groupped_value][$onegroup][$i]["y"]=$y;
 					$i=$i+1;
 			}
 		}
-		if($max_final_questions<$final_questions){
-		$max_final_questions=$final_questions;
-		}
 	}
 }
+// call the array counted to assign the values so it can be viewable in the script adding label and y keys ENDS
 
-
-foreach ($dataPointsdemo as $dPkey => $dPvalue) {
+//create arrays for groups and questions START
+foreach ($dataPoints as $dPkey => $dPvalue) {
 	//echo "- 1." . $dPkey . $dPvalue; 
 	$all_groups[]=$dPkey;
 	foreach ($dPvalue as $groupnamekey => $groupnamevalue) {
 		//echo "- 2." .$groupnamekey;
 		$allquestions[]=$groupnamekey;
-		foreach ($groupnamevalue as $gnvkey => $gnvvalue) {
-			//echo "- 3." .$gnvkey . $gnvvalue;
-			foreach ($gnvvalue as $lastkey => $lastvalue) {
-				//	echo "<hr>";
-				//echo '-4 ' . $lastkey . $lastvalue;
-				//echo "- 1." . $dPkey ." - 2." .$groupnamekey. " - 3." .$gnvkey." - 4." .$lastvalue;
-				//echo "<br>";
-			}	
-		}
 
 	}
 }
-//echo "<br> all groups : ";
-//print_r($allgroups);
+//create arrays for groups and questions ENDS
+
+//unique the groups
 $all_groups_unique=array_unique($all_groups);
-//echo "<br> all groups unique: ";
-//print_r($all_groups_unique);
+//unique the questions
 $all_questions_unique=array_unique($allquestions);
-//print_r($all_questions_unique);
 
 
+// testing purposes only STARTS
 // // echo "<br>unique groups";
 // // print_r($all_groups_unique);
 // foreach ($all_questions_unique as $questionkey => $questionvalue) {
@@ -67,14 +45,13 @@ $all_questions_unique=array_unique($allquestions);
 // 		//echo "edw arxizei to kathe group loop  ";
 // 		echo '<br>2. group : '. $groupvalue;
 // 		echo '<br> this is the final: <br> ' ;
-// 		print_r($dataPointsdemo[$groupvalue][$questionvalue]);
+// 		print_r($dataPoints[$groupvalue][$questionvalue]);
 // 		//echo "edw teleiwnei to kathe group loop  ";
 
 // 	}
 // 	//echo 'edw teleiwnei  to chart' ;
 // }
-
-//try 2 ends
+// testing purposes only ENDS
 ?>
 
 
@@ -87,7 +64,7 @@ $all_questions_unique=array_unique($allquestions);
 foreach ($all_questions_unique as $questionkey => $questionvalue) {
 	?>
 	 
-	var chart = new CanvasJS.Chart("chart-<?php echo $questionvalue ;?>", {
+	var chart<?php echo $questionvalue ;?> = new CanvasJS.Chart("chart-<?php echo $questionvalue ;?>", {
 		animationEnabled: true,
 		theme: "light2",
 		title:{
@@ -100,7 +77,7 @@ foreach ($all_questions_unique as $questionkey => $questionvalue) {
 			cursor: "pointer",
 			verticalAlign: "center",
 			horizontalAlign: "right",
-			itemclick: toggleDataSeries
+			itemclick: toggleDataSeries<?php echo $questionvalue ;?>
 		},
 		data: [
 			<?php
@@ -117,25 +94,26 @@ foreach ($all_questions_unique as $questionkey => $questionvalue) {
 					indexLabel: "{y}",
 					yValueFormatString: "#0.##",
 					showInLegend: true,
+					//if the group value title is not the total make it non visible by default
 					<?php if  ($groupvaluetitle!='Συνολικές απαντήσεις'){ ?>
 						visible: false,
 					<?php } ?>
-					dataPoints: <?php echo json_encode($dataPointsdemo[$groupvalue][$questionvalue], JSON_NUMERIC_CHECK); ?>
+					dataPoints: <?php echo json_encode($dataPoints[$groupvalue][$questionvalue], JSON_NUMERIC_CHECK); ?>
 				},
 				<?php		
 			} ?>
 		]
 	});
-	chart.render();
+	chart<?php echo $questionvalue ;?>.render();
 	 
-	function toggleDataSeries(e){
+	function toggleDataSeries<?php echo $questionvalue ;?>(e){
 		if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
 			e.dataSeries.visible = false;
 		}
 		else{
 			e.dataSeries.visible = true;
 		}
-		chart.render();
+		chart<?php echo $questionvalue ;?>.render();
 	}
 	 
 	<?php	
@@ -147,14 +125,18 @@ foreach ($all_questions_unique as $questionkey => $questionvalue) {
 <!-- script end here -->
 <?php
 
+
+//create the loop for all divs the charts will be shown STARTS
 foreach ($all_questions_unique as $questionkey => $questionvalue) {
 	?>
 	<!-- create the div for each chart -->
 	<div id="chart-<?php echo $questionvalue ; ?>" style="height: 370px; width: 100%;"></div>
 	<?php
 }
+//create the loop for all divs the charts will be shown ENDS
 ?>
 
+<!--call the canvas js script-->
 <!--<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>-->
 <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
